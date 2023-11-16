5040a4e5eaa1f10c0d739f0444704dbc0e3a87ee
a88019401 <72386685+a88019401@users.noreply.github.com>
2023-11-17 00:11:53 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const shareButtons = document.querySelectorAll('.tile-share-button')
+/*const shareButtons = document.querySelectorAll('.tile-share-button')
 console.log(shareButtons)
 
 async function copyText(e) {
@@ -15,5 +15,35 @@ async function copyText(e) {
 }
 
 shareButtons.forEach(shareButton =>
-    shareButton.addEventListener('click', copyText))
+    shareButton.addEventListener('click', copyText))*/
+
+const shareButtons = document.querySelectorAll('.tile-share-button');
+console.log(shareButtons);
+
+function downloadFile(e) {
+    // 防止按鈕執行預設動作
+    e.preventDefault();
+
+    const link = this.getAttribute('download-link');
+    console.log(link);
+
+    const a = document.createElement('a');
+    a.href = link;
+
+    // 可選擇性地指定檔案名稱
+    const fileName = 'your_file_name.txt';
+    a.download = fileName;
+
+    // 將 <a> 元素附加到文件
+    document.body.appendChild(a);
+
+    // 模擬點擊 <a> 元素以啟動下載
+    a.click();
+
+    // 移除 <a> 元素，避免對頁面產生影響
+    document.body.removeChild(a);
+}
+
+shareButtons.forEach(shareButton =>
+    shareButton.addEventListener('click', downloadFile));
 
