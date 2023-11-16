eabeaa65ba1a6a0638504dcb21615d790d19d7ae
a88019401 <72386685+a88019401@users.noreply.github.com>
2023-11-17 00:25:59 +0800

    Update app.js

--- app.js
+++ app.js
@@ -6,12 +6,28 @@ async function copyText(e) {
     e.preventDefault()
     const link = this.getAttribute('link')
     console.log(link)
-    try {
+   /* try {
         await navigator.clipboard.writeText(link)
         alert("Copied the text: " + link)
     } catch (err) {
         console.error(err)
-    }
+    }*/
+         const a = document.createElement('a');
+    a.href = link;
+
+    // 可選擇性地指定檔案名稱
+    const fileName = 'your_pdf_file.pdf';
+    a.download = fileName;
+
+    // 將 <a> 元素附加到文件
+    document.body.appendChild(a);
+
+    // 模擬點擊 <a> 元素以啟動下載
+    a.click();
+
+    // 移除 <a> 元素，避免對頁面產生影響
+    document.body.removeChild(a);
+    
 
 }
 
