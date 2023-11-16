da21babb3d4198238a352f62e884def67631ca2e
a88019401 <72386685+a88019401@users.noreply.github.com>
2023-11-17 00:17:34 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/*const shareButtons = document.querySelectorAll('.tile-share-button')
+const shareButtons = document.querySelectorAll('.tile-share-button')
 console.log(shareButtons)
 
 async function copyText(e) {
@@ -6,30 +6,18 @@ async function copyText(e) {
     e.preventDefault()
     const link = this.getAttribute('link')
     console.log(link)
-    try {
+    /*try {
         await navigator.clipboard.writeText(link)
         alert("Copied the text: " + link)
     } catch (err) {
         console.error(err)
-    }
-}
-
-shareButtons.forEach(shareButton =>
-    shareButton.addEventListener('click', copyText))*/
-
-const shareButtons = document.querySelectorAll('.tile-share-button');
-console.log(shareButtons);
-
-function downloadFile(e) {
-    // 防止按鈕執行預設動作
-    e.preventDefault();
-
-    const link = this.getAttribute('download-link');
-    console.log(link);
-
-    const a = document.createElement('a');
+    }*/
+     const a = document.createElement('a');
     a.href = link;
 
+    // 可選擇性地指定檔案名稱
+    const fileName = 'your_pdf_file.pdf';
+    a.download = fileName;
 
     // 將 <a> 元素附加到文件
     document.body.appendChild(a);
@@ -39,8 +27,8 @@ function downloadFile(e) {
 
     // 移除 <a> 元素，避免對頁面產生影響
     document.body.removeChild(a);
+
 }
 
 shareButtons.forEach(shareButton =>
-    shareButton.addEventListener('click', downloadFile));
-
+    shareButton.addEventListener('click', copyText))
